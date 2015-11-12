f90f15c1dfe3729b724a847ef268641feac43ab5
Zoran <zokicpokic@gmail.com>
2015-11-12 19:39:24 +0100

    created app
    yeoman based

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name testhmApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the testhmApp
+ */
+angular.module('testhmApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name testhmApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the testhmApp
+ */
+angular.module('testhmApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
